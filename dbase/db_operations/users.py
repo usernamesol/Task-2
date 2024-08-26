70726f948fab9d9eb59f218bd79f3f59e6798490
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-26 17:36:17 +0300

    Find user in DB
    Add check user when he registers.
    User exist if username, email or phone in db.
    If user exist return user else None.

--- dbase/db_operations/users.py
+++ dbase/db_operations/users.py
@@ -1,17 +1,30 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, or_
 from models.users import User
 from schemas.users import UserRegister
 
 
 async def create_user(user: UserRegister, db: AsyncSession):
-    user = User(
-        username=user.username,
-        password=user.password,
-        email=user.email,
-        birth_date=user.birth_date,
-        phone=user.phone,
+    stmt = select(User).where(
+        or_(
+            User.username == user.username,
+            User.email == user.email,
+            User.phone == user.phone,
+        )
     )
-    db.add(user)
-    await db.commit()
-    await db.refresh(user)
+    user_exist = await db.execute(stmt)
+    if not user_exist.fetchone():
+        user = User(
+            username=user.username,
+            password=user.password,
+            email=user.email,
+            birth_date=user.birth_date,
+            phone=user.phone,
+        )
+        db.add(user)
+        await db.commit()
+        await db.refresh(user)
+    else:
+        user = None
+
     return user
