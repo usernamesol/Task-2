c4724f9c38e7047b9738439d4ab4b56e546b3eb8
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-26 14:16:36 +0300

    Change driver psycopg2 to asyncpg

--- dbase/db_conf.py
+++ dbase/db_conf.py
@@ -10,5 +10,5 @@ class DBConfig:
     PG_SERVER = os.getenv("PG_SERVER")
     PG_PORT = os.getenv("PG_PORT")
     PG_DB_NAME = os.getenv("PG_DB")
-    PG_DB_URL = ("postgresql+psycopg2://"
+    PG_DB_URL = ("postgresql+asyncpg://"
                  f"{PG_USER}:{PG_PASSWORD}@{PG_SERVER}:{PG_PORT}/{PG_DB_NAME}")
