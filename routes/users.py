92f8a3b406245253538b56d12f16993a55390fdc
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-26 17:40:58 +0300

    Call create_user to check it
    raise HTTPException if user exist.

--- routes/users.py
+++ routes/users.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 from schemas import users
+from dbase.db_conn import get_db
+from dbase.db_operations.users import create_user
 
 
 router = APIRouter(tags=["users"])
 
 
-@router.post("/users/register")
-async def users_register(user: users.UserRegister):
+@router.post("/users/register", status_code=status.HTTP_201_CREATED)
+async def users_register(
+    user: users.UserRegister,
+    db: AsyncSession = Depends(get_db),
+):
+    user = await create_user(user, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username, email or phone already registered."
+        )
+
     return user
