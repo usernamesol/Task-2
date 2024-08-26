a52cc765b62c73d3f56444fcddda6f2ded09029e
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-26 10:39:20 +0300

    Add validation for users schemas
    Add password validation - min length = 8.
    Add username validation - min length = 5.
    Add phone validation - min digits = 3.
    Move base imports to top.

--- schemas/users.py
+++ schemas/users.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel, EmailStr
 from datetime import date
+from pydantic import BaseModel, EmailStr, Field
 
 
 class BaseUser(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(min_length=8)
 
 
 class UserRegister(BaseUser):
-    username: str
+    username: str = Field(min_length=5)
     birth_date: date
-    phone: str
+    phone: str = Field(pattern=r"[0-9]{3,}")
