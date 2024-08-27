ecb4b527ab9f6df78e13516cbd73b79accec7750
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-27 10:46:43 +0300

    Add file table
    Add file table with fiels:
    	id: int, primaty key.
    	name: str, length 255, unique.
    	user_id: int, Foreign key(User).
    Add relationship between User and File.

--- models/users.py
+++ models/users.py
@@ -1,6 +1,11 @@
 from datetime import date
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+)
 
 
 class Base(DeclarativeBase):
@@ -16,3 +21,13 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(255), unique=True)
     phone: Mapped[str] = mapped_column(unique=True)
     birth_date: Mapped[date]
+    files: Mapped[list["File"]] = relationship(back_populates="user")
+
+
+class File(Base):
+    __tablename__ = "files"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="files")
