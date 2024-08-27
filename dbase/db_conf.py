6e9c91c3df5db66cd405fbf73b1fbdc9fb5816b8
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-27 17:10:23 +0300

    Add secret key and algorithm for JWT in config file

--- dbase/db_conf.py
+++ dbase/db_conf.py
@@ -10,11 +10,13 @@ class DBConfig:
     PG_SERVER = os.getenv("PG_SERVER")
     PG_PORT = os.getenv("PG_PORT")
     PG_DB_NAME = os.getenv("PG_DB_NAME")
+    PG_TEST_DB_NAME = os.getenv("PG_TEST_DB_NAME")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
     PG_DB_URL = (
         "postgresql+asyncpg://"
         f"{PG_USER}:{PG_PASSWORD}@{PG_SERVER}:{PG_PORT}/{PG_DB_NAME}"
     )
-    PG_TEST_DB_NAME = os.getenv("PG_TEST_DB_NAME")
     PG_TEST_DB_URL = (
         "postgresql+asyncpg://"
         f"{PG_USER}:{PG_PASSWORD}@{PG_SERVER}:{PG_PORT}/{PG_TEST_DB_NAME}"
