c7dc48f10fd27a5f062d69bdaa4aa97233f3e626
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-27 22:37:36 +0300

    Create test files path
    Register user to login.
    Get token from email and password.
    Get files, using token for Authorization.

---
+++ tests/test_files.py
@@ -0,0 +1,29 @@
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.asyncio
+async def test_files_all(ac: AsyncClient):
+    # Init user to test files.
+    response = await ac.post("/users/register", json={
+        "username": "usertestfile",
+        "password": "usertpasswordfile",
+        "email": "testfile@mail.ru",
+        "birth_date": "2000-01-01",
+        "phone": "38 542 5888 232",
+    })
+    assert response.status_code == 201
+
+    # Get token.
+    response = await ac.post("/users/login", json={
+        "email": "testfile@mail.ru",
+        "password": "usertpasswordfile",
+    })
+    assert response.status_code == 200
+    token = "Bearer " + response.json()["Your token"]
+
+    response = await ac.get("/files/all", headers={
+        "Authorization": token,
+    })
+    assert response.status_code == 200
+    assert response.json() == {"Message": "You don't have any files."}
