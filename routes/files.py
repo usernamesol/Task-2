3985402cb4642fba14d8446623fa2d45b1a7ee1b
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-28 21:49:03 +0300

    Add get file information
    Add func files_get_info to get and return file info.
    Return 404 error if file not found.
    File info: last modified, size(MB).

--- routes/files.py
+++ routes/files.py
@@ -1,3 +1,5 @@
+import os
+from datetime import datetime
 from fastapi import (
     APIRouter,
     status,
@@ -8,10 +10,12 @@ from fastapi import (
     BackgroundTasks
 )
 from sqlalchemy.ext.asyncio import AsyncSession
+from dbase.db_conf import DBConfig
 from dbase.db_conn import get_db
 from dbase.db_operations.users import get_user_from_db
 from dbase.db_operations.files import (
     get_files_from_db,
+    get_file_from_db,
     get_file_content,
     save_file,
     save_file_to_db,
@@ -64,3 +68,32 @@ async def files_upload(
     )
 
     return {"Message": f"File: {file_name} uploaded sucessfully."}
+
+
+@router.get("/files/{file_name}", status_code=status.HTTP_200_OK)
+async def files_get_info(
+    file_name: str,
+    user: str = Depends(get_user_from_token),
+    db: AsyncSession = Depends(get_db),
+):
+    user_db = await get_user_from_db(user, db)
+    user_db = user_db._tuple()[0]
+    file_db = await get_file_from_db(user_db.id, file_name, db)
+    if not file_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"File: {file_name} not found.",
+        )
+
+    file_path = DBConfig.USER_FILES_PATH + user_db.username
+    file_path = os.path.join(file_path, file_name)
+    mtime = os.path.getmtime(file_path)
+    mtime_readable = datetime.fromtimestamp(mtime)
+    file_size_mb = round(os.path.getsize(file_path) / 1024 ** 2, 2)
+    response = {
+        "file": file_name,
+        "last modified": mtime_readable,
+        "size": file_size_mb,
+    }
+
+    return response
