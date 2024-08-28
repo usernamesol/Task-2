692e3ada9dce1460f3ec2d259bda39e590ada37c
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-28 17:19:15 +0300

    Save upload files
    Create user dir for its files.
    Get content from file.
    Save file to users dir.
    Save file name to DB.

--- dbase/db_operations/files.py
+++ dbase/db_operations/files.py
@@ -1,6 +1,9 @@
+import os
+from fastapi import UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from models.users import File
+from dbase.db_conf import DBConfig
+from models.users import File, User
 
 
 async def get_files_from_db(user_id: int, db: AsyncSession):
@@ -9,3 +12,55 @@ async def get_files_from_db(user_id: int, db: AsyncSession):
     files = files.fetchall()
 
     return files
+
+
+async def check_file(
+    user_id: int,
+    file_name: str,
+    db: AsyncSession,
+) -> bool:
+    stmt = select(File).where(
+        File.user_id == user_id,
+        File.name == file_name,
+    )
+    file = await db.execute(stmt)
+    file = file.first()
+
+    return True if file else False
+
+
+async def save_file_to_db(
+    user_db: User,
+    file_name: str,
+    db: AsyncSession,
+):
+    file_exist = await check_file(user_db.id, file_name, db)
+    if not file_exist:
+        file_db = File(
+            name=file_name,
+            user=user_db,
+        )
+        db.add(file_db)
+        await db.commit()
+        await db.refresh(file_db)
+    else:
+        file_db = None
+
+    return file_db
+
+
+async def get_file_content(file: UploadFile):
+    return await file.read()
+
+
+async def save_file(
+    file_name: str,
+    file_content: bytes,
+    username: str,
+):
+    path = DBConfig.USER_FILES_PATH + username
+    os.makedirs(path, exist_ok=True)
+    path = os.path.join(path, file_name)
+
+    with open(path, "wb") as file:
+        file.write(file_content)
