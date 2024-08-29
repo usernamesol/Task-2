8e27983f5367f1f1f41e3a75c79d2d8b868e256c
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-29 13:29:16 +0300

    Add Test path and file in config
    Add test files path for save test file.
    Add test file for test.

--- dbase/db_conf.py
+++ dbase/db_conf.py
@@ -14,6 +14,8 @@ class DBConfig:
     SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = os.getenv("ALGORITHM")
     USER_FILES_PATH = os.getenv("USER_FILES_PATH")
+    TEST_FILES_PATH = os.getenv("TEST_FILES_PATH")
+    TEST_FILE = os.getenv("TEST_FILE")
     PG_DB_URL = (
         "postgresql+asyncpg://"
         f"{PG_USER}:{PG_PASSWORD}@{PG_SERVER}:{PG_PORT}/{PG_DB_NAME}"
