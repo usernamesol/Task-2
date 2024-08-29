305c92b6a1dd89feba7f344ea681052e622b0824
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-29 13:41:23 +0300

    Add test files upload
    Add test with first correct request.
    Add test with second incorrect(the same file) request.
    Delete test all files.

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,17 +1,22 @@
 import pytest
 from httpx import AsyncClient
+from dbase.db_conf import DBConfig
 
 
 @pytest.mark.asyncio
-async def test_files_all(ac: AsyncClient):
+async def test_files_upload(ac: AsyncClient):
+    global token
     # Init user to test files.
-    response = await ac.post("/users/register", json={
-        "username": "usertestfile",
-        "password": "usertpasswordfile",
-        "email": "testfile@mail.ru",
-        "birth_date": "2000-01-01",
-        "phone": "38 542 5888 232",
-    })
+    response = await ac.post(
+        url="/users/register",
+        json={
+            "username": "usertestfile",
+            "password": "usertpasswordfile",
+            "email": "testfile@mail.ru",
+            "birth_date": "2000-01-01",
+            "phone": "38 542 5888 232",
+        }
+    )
     assert response.status_code == 201
 
     # Get token.
@@ -22,15 +27,19 @@ async def test_files_all(ac: AsyncClient):
     assert response.status_code == 200
     token = "Bearer " + response.json()["Your token"]
 
-    response = await ac.get("/files/all", headers={
-        "Authorization": token,
-    })
-    assert response.status_code == 200
-    assert response.json() == {"Message": "You don't have any files."}
+    with open(DBConfig.TEST_FILE, "rb") as file:
+        response = await ac.post(
+            url="/files/upload",
+            headers={"Authorization": token},
+            files={"file": file},
+        )
+    assert response.status_code == 201
 
-    bad_token = "Bearer test"
-    response = await ac.get("/files/all", headers={
-        "Authorization": bad_token
-    })
-    assert response.status_code == 403
-    assert response.json() == {"detail": "Bad token."}
+    # Repeat upload the same file
+    with open(DBConfig.TEST_FILE, "rb") as file:
+        response = await ac.post(
+            url="/files/upload",
+            headers={"Authorization": token},
+            files={"file": file},
+        )
+    assert response.status_code == 400
