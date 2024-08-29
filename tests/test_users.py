c890b0ef21e17e920e6e8bffa39aa16aa8dfbe5d
Solevoy <dmitriy.valiev.2016@gmail.com>
2024-08-29 14:50:47 +0300

    Fix unreadable params

--- tests/test_users.py
+++ tests/test_users.py
@@ -35,16 +35,22 @@ async def test_users_register(ac: AsyncClient):
 
 @pytest.mark.asyncio
 async def test_users_login(ac: AsyncClient):
-    response = await ac.post("/users/login", json={
-        "email": "test@mail.ru",
-        "password": "usertpassword",
-    })
+    response = await ac.post(
+        url="/users/login",
+        json={
+            "email": "test@mail.ru",
+            "password": "usertpassword",
+        }
+    )
     assert response.status_code == 200
 
-    response = await ac.post("/users/login", json={
-        "email": "bad@mail.ru",
-        "password": "fakepassword",
-    })
+    response = await ac.post(
+        url="/users/login",
+        json={
+            "email": "bad@mail.ru",
+            "password": "fakepassword",
+        }
+    )
     assert response.status_code == 403
     assert response.json() == {
         "detail": "Wrong email or password"
